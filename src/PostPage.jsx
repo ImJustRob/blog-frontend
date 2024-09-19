5c122c64ea1c4bac890f2dcb29edc4365bbbe91a
ImJustRob <robertalothian@hotmail.com>
2024-09-18 21:42:49 -0400

    using useEffect

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,22 +1,25 @@
 import { PostIndex } from "./PostIndex";
 import { PostNew } from "./PostNew";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function PostPage() {
     let name = "Test";
-    let posts = [];
+    const [posts, setPosts] = useEffect(handleIndex, []);
 
   const handleIndex = () => {
     axios.get("http://localhost:3000/posts.json").then(responce => {
       console.log(responce);
+      setPosts(responce.data)
     })
   }
+  useEffect(setPosts,[])
   
     return (
       <main>
         <PostNew />
         <PostIndex name={name} posts={posts} />
-        <button onClick={handleIndex}>get the data</button>
+        {/* <button onClick={handleIndex}>get the data</button> */}
       </main>
     )
   }
