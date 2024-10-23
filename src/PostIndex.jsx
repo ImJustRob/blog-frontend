0e8dfe0c5667c9b61334c257d34ad1b862f6b6f1
ImJustRob <robertalothian@hotmail.com>
2024-10-22 20:37:13 -0400

    altered postindex page to make it functional

--- src/PostIndex.jsx
+++ src/PostIndex.jsx
@@ -1,20 +1,7 @@
-export function PostIndex(props) {
-    // console.log(props);
-    return (
-      <div id="posts-index">
-      <h1>All post</h1>
-      <div className="cards"> 
-      {props.posts.map(post => (
-          <div key={post.id} className="posts">
-            <h2>{post.title}</h2>
-            <img src={post.image} alt="" />
-            <p> {post.body}</p>
-            <button onClick={props.onShow(post)}>More info</button>
-          </div>
-        ))}
-        </div>
-  
-  
-  </div>    
-    )
-  }
+export function PostIndex() {
+  return (
+    <div>
+      <h1>All photos</h1>
+    </div>
+  );
+}
